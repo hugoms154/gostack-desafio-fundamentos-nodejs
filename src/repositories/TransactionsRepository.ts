a85eb805ad53ac7fd18452a7e5279e8ebb801818
hugo miranda <hugo.miranda.souza@gmail.com>
2020-08-25 18:27:42 -0300

    feat: create, list, balance
    features: 
    Create a transaction,
    Balance (incomes - outcomes),
    Lists all transactions and then shows the current balance

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,23 +6,61 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
   constructor() {
     this.transactions = [];
+    // fakes.forEach(fake => this.transactions.push(fake));
   }
 
   public all(): Transaction[] {
     // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
     // TODO
+    const incomes: number[] = [];
+    let income = 0;
+    const outcomes: number[] = [];
+    let outcome = 0;
+
+    this.transactions.forEach(({ type, value }) => {
+      return type === 'income' ? incomes.push(value) : outcomes.push(value);
+    });
+
+    if (incomes.length > 0) {
+      income = incomes.reduce((first, second) => {
+        const total: number = first + second;
+        return total;
+      });
+    }
+    if (outcomes.length > 0) {
+      outcome = outcomes.reduce((first, second) => {
+        const total: number = first + second;
+        return total;
+      });
+    }
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
     // TODO
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,33 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
+  public execute({ title, value, type }: Request): Transaction {
     // TODO
+    let balanceEnoughToOutcome = 0;
+    if (type === 'outcome') {
+      balanceEnoughToOutcome =
+        this.transactionsRepository.getBalance().total - value;
+    }
+    if (!title) {
+      throw Error('Title field cannot be empty!');
+    }
+    if (value <= 0 || !value) {
+      throw Error('Value field must be higher than 0');
+    }
+    if (!type) {
+      throw Error("Type field must be type 'income' or 'outcome'");
+    }
+    if (balanceEnoughToOutcome < 0) {
+      throw Error('You dont have enough money to this outcome');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
